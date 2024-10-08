7dfe307f73c3dc95079978682df6e3faf49f23c9
narandza <60319154+narandza@users.noreply.github.com>
2024-10-08 13:58:32 +0200

    create sidebar item component, create sidebar look

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,15 @@
 import { cn } from "@/lib/utils";
+import Mascot from "@/public/mascot.svg";
+import Image from "next/image";
+
+import Link from "next/link";
+import HomeIcon from "@/public/learn.svg";
+import LeaderboardIcon from "@/public/leaderboard.svg";
+import ShopIcon from "@/public/shop.svg";
+import QuestsIcon from "@/public/quests.svg";
+import { SidebarItem } from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -12,7 +23,32 @@ export const Sidebar = ({ className }: Props) => {
         className
       )}
     >
-      sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-4 flex items-center gap-x-3">
+          <Image src={Mascot} height={40} width={40} alt="mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            LingoQuest
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="learn" href="/learn" iconSrc={HomeIcon} />
+        <SidebarItem
+          label="leaderboard"
+          href="/leaderboard"
+          iconSrc={LeaderboardIcon}
+        />
+        <SidebarItem label="quests" href="/quests" iconSrc={QuestsIcon} />
+        <SidebarItem label="shop" href="/shop" iconSrc={ShopIcon} />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton></UserButton>
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
