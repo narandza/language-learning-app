6b7fad648df9aaedb6bc0d1f095e503106dd6238
narandza <60319154+narandza@users.noreply.github.com>
2024-10-08 11:30:49 +0200

    create landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+
+import Image from "next/image";
+import Hero from "../../hero.svg";
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
-      Marketing page
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src={Hero} alt="Hero" fill />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice, and master new languages with LingoQuest.
+        </h1>
+        <div className="">
+          <ClerkLoading>
+            <Loader className="size-5 animate-spin text-muted-foreground" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal">
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton>
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   );
 }
