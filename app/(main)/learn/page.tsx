65e81ba9ff96729e6af706d638dc49cb7645930b
narandza <60319154+narandza@users.noreply.github.com>
2024-10-10 19:17:27 +0200

    fix queries syntax

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -17,11 +17,7 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{
-            id: userProgress.activeCourse.id ?? undefined,
-            title: userProgress.activeCourse.title ?? "No title available",
-            imageSrc: userProgress.activeCourse.imageSrc ?? "",
-          }}
+          activeCourse={userProgress.activeCourse}
           hearts={userProgress.hearts}
           points={userProgress.points}
           hasActiveSubscription={false}
