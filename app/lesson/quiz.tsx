b794140e5947d43a366663bb7457649165c3cec8
narandza <60319154+narandza@users.noreply.github.com>
2024-10-11 11:25:14 +0200

    create lesson page, layoput and loading; create lesson header and quiz component; add shadcn progress component & change default color; fix getUnits bug;

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { challengeOptions, challenges } from "@/db/schema";
+import { useState } from "react";
+import { Header } from "./header";
+
+type Props = {
+  initialLessonId: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferInsert)[];
+  })[];
+  initialHearts: number;
+  initialPercentage: number;
+  userSubscription: undefined; // TODO: Replace with subscription DB type
+};
+
+export const Quiz = ({
+  initialLessonId,
+  initialLessonChallenges,
+  initialHearts,
+  initialPercentage,
+  userSubscription,
+}: Props) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialPercentage);
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={50 || percentage}
+        // hasActiveSubscription={!!userSubscription?.isActive}
+      />
+    </>
+  );
+};
