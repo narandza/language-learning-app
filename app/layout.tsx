0dae447985d947895e46d0f83e7802f098c7db39
narandza <60319154+narandza@users.noreply.github.com>
2024-10-11 11:57:16 +0200

    create exit modal component, fix header formating, implement exit modal in lesson page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import { ExitModal } from "@/components/modals/exit-modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           {children}
 
           <Toaster />
+          <ExitModal />
         </body>
       </html>
     </ClerkProvider>
