10d6a0ffb942c79d194863aab1d07e9ec195aafc
narandza <60319154+narandza@users.noreply.github.com>
2024-10-12 13:10:41 +0200

    create challenge progress action, create onNext&onContinue fucntions in lesson quiz

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -41,10 +41,42 @@ export const Quiz = ({
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions;
 
+  const onNext = () => {
+    setActiveIndex((current) => current + 1);
+  };
+
   const onSelect = (id: number) => {
     if (status !== "none") return;
 
     setSelectedOption(id);
+  };
+
+  const onContinue = () => {
+    if (!selectedOption) return;
+
+    if (status === "wrong") {
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+
+    if (status === "correct") {
+      onNext();
+      setSelectedOption(undefined);
+      return;
+    }
+
+    const correctOption = options.find((option) => option.correct);
+
+    if (!correctOption) {
+      return;
+    }
+
+    if (correctOption.id === selectedOption) {
+      console.log("correct option");
+    } else {
+      console.error("incorrect option");
+    }
   };
 
   const title =
@@ -80,7 +112,7 @@ export const Quiz = ({
           </div>
         </div>
       </div>
-      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
+      <Footer disabled={!selectedOption} status={status} onCheck={onContinue} />
     </>
   );
 };
