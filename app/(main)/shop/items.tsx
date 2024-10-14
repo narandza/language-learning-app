24525b1c70bcc17c2f147d140afab17fc82f5121
narandza <60319154+narandza@users.noreply.github.com>
2024-10-14 12:45:53 +0200

    create refil hearts action

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { refillHearts } from "@/actions/user-progress";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useTransition } from "react";
+import { toast } from "sonner";
 
 const POINTS_TO_REFILL = 10;
 
@@ -19,7 +21,9 @@ export const Items = ({ hearts, points, hasActiveSubscription }: Props) => {
       return;
     }
 
-    startTransition(() => {});
+    startTransition(() => {
+      refillHearts().catch(() => toast.error("Something went wrong."));
+    });
   };
 
   return (
