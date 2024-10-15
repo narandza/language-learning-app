e77e12a7b3d9dff756278d082183ca7a698e0178
narandza <60319154+narandza@users.noreply.github.com>
2024-10-16 00:05:11 +0200

    create leaderboard page;create leaderboard loading;create shop loading;

---
+++ app/(main)/leaderboard/page.tsx
@@ -0,0 +1,54 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { getUserProgress, getUserSubscription } from "@/db/queries";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+
+const LeaderboardPage = async () => {
+  const userProgressData = getUserProgress();
+  const userSubscriptionData = getUserSubscription();
+
+  const [userProgress, userSubscription] = await Promise.all([
+    userProgressData,
+    userSubscriptionData,
+  ]);
+
+  const isPro = !!userSubscription?.isActive;
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  return (
+    <div className="flex flex-row-reverse gap-[48px]">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={isPro}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <div className="w-full flex flex-col items-center">
+          <Image
+            src="/leaderboard.svg"
+            alt="Leaderboard"
+            height={90}
+            width={90}
+          />
+          <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
+            Leaderboard
+          </h1>
+          <p className="text-muted-foreground text-center text-lg mb-6">
+            See where you stand among other learners in the community.
+          </p>
+          {/* TODO: Add User List */}
+        </div>
+      </FeedWrapper>
+    </div>
+  );
+};
+
+export default LeaderboardPage;
