6cfbe811d0e59a984333de97395ce2a42d226a8b
narandza <60319154+narandza@users.noreply.github.com>
2024-10-16 15:51:52 +0200

    create challenges crud admin operations

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -15,6 +15,10 @@ import { LessonList } from "./lesson/list";
 import { LessonCreate } from "./lesson/create";
 import { LessonEdit } from "./lesson/edit";
 
+import { ChallengeList } from "./challenge/list";
+import { ChallengeCreate } from "./challenge/create";
+import { ChallengeEdit } from "./challenge/edit";
+
 const dataProvider = simpleRestProvider("/api");
 
 const App = () => {
@@ -39,6 +43,13 @@ const App = () => {
         list={LessonList}
         create={LessonCreate}
         edit={LessonEdit}
+        recordRepresentation="title"
+      />
+      <Resource
+        name="challenges"
+        list={ChallengeList}
+        create={ChallengeCreate}
+        edit={ChallengeEdit}
         recordRepresentation="title"
       />
     </Admin>
